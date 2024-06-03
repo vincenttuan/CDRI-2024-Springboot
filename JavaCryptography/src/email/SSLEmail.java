b7af294b93ec6dfe2989b6ea9036c71af6820a6b
NTPU <NTPU@503-tr>
2024-06-03 14:50:19 +0800

    java

--- JavaCryptography/src/email/SSLEmail.java
+++ JavaCryptography/src/email/SSLEmail.java
@@ -7,11 +7,13 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 /*
- * 申請: 應用程式密碼
+ * 申請: Gmail 應用程式密碼
  * 1. 登錄你的 Google 帳戶。
  * 2. 前往 https://myaccount.google.com/security
  * 3. 點選"兩步驟驗證"
  * 4. 點選"應用程式密碼" <-- 網頁最下方
+ * ps: 若看不到"應用程式密碼", 請點選下方網址
+ * https://myaccount.google.com/apppasswords?pli=1&rapt=AEjHL4N4imft3Utjxqp0tmG-NW8zedKzQoKwtNqkPkUFd7BpnC6Se5HtNGnKDfzP3x5UpSPgyfjuEHAMNY27rVhsV0gOr33vN9s7IkYMr-EurprkV-L0mvE
  * 5. 這將生成一個應用程序專用密碼，複製此密碼。
  * */
 
@@ -25,7 +27,7 @@ public class SSLEmail {
 	 */
 	public static void main(String[] args) {
 		final String fromEmail = "xxx@gmail.com"; //requires valid gmail id
-		final String password = "xxxx"; // 應用程式密碼
+		final String password = "ovva jhpt cvyp cxwd"; // 應用程式密碼
 		final String toEmail = "xxx@yahoo.com.tw"; // can be any email id 
 		
 		System.out.println("SSLEmail Start");
