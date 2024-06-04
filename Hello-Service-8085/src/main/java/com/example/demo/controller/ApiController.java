ca97056f2fb2c34696c69fe657433871cfe42bb1
NTPU <NTPU@503-tr>
2024-06-04 11:22:02 +0800

    hello

---
+++ Hello-Service-8085/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+	
+	@Autowired
+	private EurekaClient eurekaClient;
+	
+	@GetMapping("/down")
+	public String down() {
+		InstanceInfo instanceInfo = eurekaClient.getApplicationInfoManager().getInfo();
+		instanceInfo.setStatus(InstanceInfo.InstanceStatus.DOWN);
+		return "DOWN";
+	}
+	
+}
