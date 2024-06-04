72fe0920b45e1f7c42115c4602c8ef98cd2ca0c6
NTPU <NTPU@503-tr>
2024-06-04 10:30:15 +0800

    java

--- Hello-Service-8085/src/main/java/com/example/demo/config/Config.java
+++ Hello-Service-8085/src/main/java/com/example/demo/config/Config.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import java.util.Map;
+
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +12,15 @@ public class Config {
 	@Bean
 	public InfoContributor appInfoContributor() {
 		InfoContributor info = builder -> {
-			builder.withDetail("app", "Say Hello & Time");
+			//builder.withDetail("app", "Say Hello & Time");
+			builder.withDetail("app", 
+					Map.of(
+							"name", "Hello App",
+							"description", "Say Hello & Time",
+							"version", "1.0.0",
+							"author", "John",
+							"last_updated", "2024-06-04"
+					));
 		};
 		
 		return info; 
