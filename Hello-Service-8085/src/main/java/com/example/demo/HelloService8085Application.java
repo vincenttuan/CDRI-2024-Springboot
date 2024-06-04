0f091da6f321e5e80f4dabde3b33647ccad11726
NTPU <NTPU@503-tr>
2024-06-04 10:01:58 +0800

    java

---
+++ Hello-Service-8085/src/main/java/com/example/demo/HelloService8085Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloService8085Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloService8085Application.class, args);
+	}
+
+}
