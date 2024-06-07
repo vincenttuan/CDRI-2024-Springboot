33ba3c14f41f1be9ed935fd7ca597c2055d748ef
NTPU <NTPU@host.docker.internal>
2024-06-07 11:49:07 +0800

    item

--- Feign-order-service-9093/src/main/java/com/example/demo/model/po/Item.java
+++ Feign-order-service-9093/src/main/java/com/example/demo/model/po/Item.java
@@ -1,5 +1,22 @@
 package com.example.demo.model.po;
 
-public class Item { // 訂單項目(明細)
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Item { // 訂單項目(明細)
+	private Integer id; // 訂單項目編號
+	private Integer orderId; // 訂單主檔編號
+	private Integer productId; // 商品編號
+	private Integer quantity; // 訂購數量
+	private Integer subtotal; // 小計
 }
