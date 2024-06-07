06ef99d61961baa256845fe276077053f84f66a6
NTPU <NTPU@host.docker.internal>
2024-06-07 11:53:02 +0800

    item

--- Feign-order-service-9093/src/main/java/com/example/demo/model/dto/ItemDto.java
+++ Feign-order-service-9093/src/main/java/com/example/demo/model/dto/ItemDto.java
@@ -1,5 +1,13 @@
 package com.example.demo.model.dto;
 
-public class ItemDto {
+import com.example.demo.model.po.Product;
 
+import lombok.Data;
+
+@Data
+public class ItemDto {
+	private Integer id; // 訂單明細編號
+	private Integer quantity; // 訂購數量
+	private Integer subtotal; // 小計
+	private Product product; // 商品資料
 }
