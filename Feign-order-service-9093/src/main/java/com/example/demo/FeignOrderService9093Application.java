0f75cf2b550cfc7595b6d8c18f898f307027a57d
NTPU <NTPU@host.docker.internal>
2024-06-07 12:31:02 +0800

    java

--- Feign-order-service-9093/src/main/java/com/example/demo/FeignOrderService9093Application.java
+++ Feign-order-service-9093/src/main/java/com/example/demo/FeignOrderService9093Application.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+// 設定 Feign Client 位置
+@EnableFeignClients(basePackages = "com.example.demo.client")
 public class FeignOrderService9093Application {
 
 	public static void main(String[] args) {
