b45483d8f604aa61eb38418944678b67163c2fb9
NTPU <NTPU@host.docker.internal>
2024-06-07 11:46:36 +0800

    order

--- Feign-order-service-9093/src/main/java/com/example/demo/model/po/Order.java
+++ Feign-order-service-9093/src/main/java/com/example/demo/model/po/Order.java
@@ -1,5 +1,23 @@
 package com.example.demo.model.po;
 
-public class Order { // 訂單主檔
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order { // 訂單主檔
+	private Integer id; // 訂單編號
+	private Integer customerId; // 客戶編號
+	private String orderDate; // 訂單日期
+	private List<Item> items = new CopyOnWriteArrayList<>(); // 訂單項目(明細)
+	private Integer total; // 總計
 }
