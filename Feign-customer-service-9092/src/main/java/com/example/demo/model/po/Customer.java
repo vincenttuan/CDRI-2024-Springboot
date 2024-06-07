02278ff63ac8d9f5a4492d10b1e72129c0415a03
NTPU <NTPU@host.docker.internal>
2024-06-07 11:17:05 +0800

    customer

--- Feign-customer-service-9092/src/main/java/com/example/demo/model/po/Customer.java
+++ Feign-customer-service-9092/src/main/java/com/example/demo/model/po/Customer.java
@@ -1,5 +1,17 @@
 package com.example.demo.model.po;
 
-public class Customer {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer { // 客戶
+	private Integer id; // 客戶編號
+	private String name; // 客戶名稱
+	private String email; // 客戶 email
 
 }
