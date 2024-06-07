70ac57c9230c1f352fc500482e620ac26ee51692
NTPU <NTPU@host.docker.internal>
2024-06-07 10:23:58 +0800

    ApiResponse

--- Feign-product-service-9091/src/main/java/com/example/demo/model/response/ApiResponse.java
+++ Feign-product-service-9091/src/main/java/com/example/demo/model/response/ApiResponse.java
@@ -1,5 +1,16 @@
 package com.example.demo.model.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> { // API 回應
+	private Boolean success;  // 是否成功
+	private String message;   // 返回訊息
+	private T data;           // 返回資料
 }
