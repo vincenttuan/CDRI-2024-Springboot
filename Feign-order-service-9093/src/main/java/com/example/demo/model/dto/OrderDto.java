a3d20ee9375403ffa71e15e2189a72c7cbafe946
NTPU <NTPU@host.docker.internal>
2024-06-07 11:51:08 +0800

    orderdto

--- Feign-order-service-9093/src/main/java/com/example/demo/model/dto/OrderDto.java
+++ Feign-order-service-9093/src/main/java/com/example/demo/model/dto/OrderDto.java
@@ -1,5 +1,16 @@
 package com.example.demo.model.dto;
 
-public class OrderDto {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import com.example.demo.model.po.Customer;
+
+import lombok.Data;
+
+@Data
+public class OrderDto {
+	private Integer id; // 訂單編號
+	private String orderDate; // 訂單日期
+	private Customer customer; // 客戶資料
+	private List<ItemDto> itemDtos = new CopyOnWriteArrayList<>(); // 訂單細目
 }
