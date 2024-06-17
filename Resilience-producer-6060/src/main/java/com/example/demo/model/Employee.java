c65e229a1747a7bfc274b8699a0a78c7a57f1015
NTPU <NTPU@host.docker.internal>
2024-06-17 11:12:51 +0800

    java

---
+++ Resilience-producer-6060/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private Integer empId;
+	private String empName;
+	private String designation;
+	private Double salary;
+}
