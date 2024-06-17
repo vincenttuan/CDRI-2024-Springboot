5b3059a6d31d701a40dee088f2bcdb16116345d9
NTPU <NTPU@host.docker.internal>
2024-06-17 09:57:42 +0800

    java

--- Gateway-Server-80/src/main/java/com/example/demo/config/GatewayConfig.java
+++ Gateway-Server-80/src/main/java/com/example/demo/config/GatewayConfig.java
@@ -2,11 +2,13 @@ package com.example.demo.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
 	
+	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route("Feign-customer-service-9092", r -> r.path("/customers/**").uri("lb://Feign-customer-service-9092"))
