4a9649b28eab7eb6bb0d81db8b9ec0994f3bc4a3
NTPU <NTPU@host.docker.internal>
2024-06-20 11:34:08 +0800

    j

--- Resilience-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Resilience-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
@@ -63,6 +64,33 @@ public class EmployeeController {
 		if(empId >= 10) {
 			throw new RuntimeException("資料庫或網路繁忙");
 		}
+		
+		Employee emp = new Employee();
+		emp.setEmpId(empId);
+		emp.setEmpName("John");
+		emp.setDescription("Manager");
+		emp.setSalary(15_0000);
+		return emp;
+	}
+	
+	// Bilkhead semaphone 隔離信號
+	@Bulkhead(name = "employeeBulkhead", fallbackMethod = "getEmployeeFallback")
+	@GetMapping("/semaphone/{empId}")
+	public Employee getEmployee4(@PathVariable Integer empId) throws InterruptedException {
+		// empId 不正確
+		if(empId <= 0) {
+			throw new RuntimeException("員工編號不正確, 無此員工");
+		}
+		
+		// 假設 empId >= 10 都會發生異常
+		if(empId >= 10) {
+			throw new RuntimeException("資料庫或網路繁忙");
+		}
+		
+		// 模擬業務處理遞延
+		// 給 @Bulkhead 使用
+		// 當執行 http://localhost:6060/employee/semaphone/1 過多時就會印出 "Bulkhead call Rejected"
+		Thread.sleep(3000);
 		
 		Employee emp = new Employee();
 		emp.setEmpId(empId);
