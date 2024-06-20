0e06f795e85058cebb1629c9eb3d849526f5fdb2
NTPU <NTPU@host.docker.internal>
2024-06-20 10:16:57 +0800

    j

--- Resilience-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Resilience-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -14,6 +14,10 @@ public class EmployeeController {
 	@GetMapping("/{empId}")
 	public Employee getEmployee(@PathVariable Integer empId) {
 		
+		if(empId <= 0) {
+			throw new RuntimeException("員工編號不正確, 無此員工");
+		}
+		
 		Employee emp = new Employee();
 		emp.setEmpId(empId);
 		emp.setEmpName("John");
