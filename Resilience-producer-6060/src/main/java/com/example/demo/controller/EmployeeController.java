67ff43717406b9c82c452d6aca2bfddd3ffbc366
NTPU <NTPU@host.docker.internal>
2024-06-21 09:45:08 +0800

    j

--- Resilience-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Resilience-producer-6060/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import com.example.demo.model.Employee;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 @RequestMapping("/employee")
@@ -134,6 +135,24 @@ public class EmployeeController {
 			return emp;
 		});
 		
+	}
+	
+	// 利用 TimeLimiter 來處理錯誤
+	@GetMapping("/timelimiter/{empId}")
+	@TimeLimiter(name = "employeeTimeLimiter", fallbackMethod = "getEmployeeFallback")
+	public Employee getEmployee6(@PathVariable Integer empId) throws InterruptedException {
+		if(empId <= 0) {
+			throw new RuntimeException("員工編號不正確, 無此員工");
+		}
+		
+		Thread.sleep(3000);
+		
+		Employee emp = new Employee();
+		emp.setEmpId(empId);
+		emp.setEmpName("John");
+		emp.setDescription("Manager");
+		emp.setSalary(15_0000);
+		return emp;
 	}
 	
 	// 這是一個回退方法(Fallback), 當 getEmployee 方法發生異常將調用此方法
