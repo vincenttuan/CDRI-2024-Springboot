e70de06a280b6d301e276250b8a627eda2de7e30
NTPU <NTPU@host.docker.internal>
2024-06-24 15:16:09 +0800

    tx

---
+++ TX-Transaction-8080/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.BookOneService;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+	
+	@Autowired
+	private BookOneService bookOneService;
+	
+	// 買單本書
+	@GetMapping("/one")
+	public String buyOneBook(@RequestParam("username") String username, @RequestParam("bookId") Integer bookId) {
+		bookOneService.buyOne(username, bookId);
+		return String.format("Buy one book OK, username:%s bookId:%d", username, bookId);
+	}
+	
+}
