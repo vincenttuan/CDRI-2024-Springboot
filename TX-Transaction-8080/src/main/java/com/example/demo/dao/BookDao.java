b4c14a2215bcf3e852c238c9047c882b3114d8ff
NTPU <NTPU@host.docker.internal>
2024-06-24 12:25:27 +0800

    tx

---
+++ TX-Transaction-8080/src/main/java/com/example/demo/dao/BookDao.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+public interface BookDao {
+	
+	Integer getBookPrice(Integer bookId); // 取得書本價格
+	Integer getBookStock(Integer bookId); // 取得書本庫存
+	Integer getWalletBalance(String username); // 取得目前客戶餘額
+	
+	Integer reduceBookStock(Integer BookId, Integer amountToReduce); // 更新書本庫存(減量)
+	
+	Integer reduceWallBalance(String username, Integer bookPrice); // 更新錢包餘額(減量)
+	
+}
