e651b66863bfe58bf3d6df4c7ab70bc66cbb7638
NTPU <NTPU@host.docker.internal>
2024-06-24 15:10:10 +0800

    tx

---
+++ TX-Transaction-8080/src/main/java/com/example/demo/service/BuyOneServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.BookDao;
+
+@Service
+public class BuyOneServiceImpl implements BookOneService {
+	
+	@Autowired
+	private BookDao bookDao;
+	
+	@Override
+	public void buyOne(String username, Integer bookId) {
+		// 1. 查詢價格
+		Integer bookPrice = bookDao.getBookPrice(bookId);
+		// 2. 減去書本庫存
+		bookDao.reduceBookStock(bookId, 1);
+		// 3. 修改客戶餘額
+		bookDao.reduceWalletBalance(username, bookPrice);
+	}
+	
+}
