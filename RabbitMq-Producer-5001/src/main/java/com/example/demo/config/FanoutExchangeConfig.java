cbfaac8662d9c93f6072675f3182a50b28df769f
NTPU <NTPU@host.docker.internal>
2024-06-26 11:34:22 +0800

    java

--- RabbitMq-Producer-5001/src/main/java/com/example/demo/config/FanoutExchangeConfig.java
+++ RabbitMq-Producer-5001/src/main/java/com/example/demo/config/FanoutExchangeConfig.java
@@ -1,5 +1,12 @@
 package com.example.demo.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /*
                                          +---------------+
                                      +-> | fanout-queue1 | --> receiveFanoutQueue1 (consumer)
@@ -9,6 +16,41 @@ sendFanout --> | fanout-exchange | --+
                                      +-> | fanout-queue2 | --> receiveFanoutQueue2 (consumer)
                                          +---------------+
 */
+@Configuration
 public class FanoutExchangeConfig {
-
+	
+	@Bean
+	public FanoutExchange fanoutExchange() {
+		return new FanoutExchange("fanout-exchange");
+	}
+	
+	@Bean
+	public Queue fanoutQueue1() {
+		return new Queue("fanout-queue1");
+	}
+	
+	@Bean
+	public Queue fanoutQueue2() {
+		return new Queue("fanout-queue2");
+	}
+	
+	@Bean
+	public Binding fanoutBinding1(FanoutExchange fanoutExchange, Queue fanoutQueue1) {
+		return BindingBuilder.bind(fanoutQueue1).to(fanoutExchange);
+	}
+	
+	@Bean
+	public Binding fanoutBinding2(FanoutExchange fanoutExchange, Queue fanoutQueue2) {
+		return BindingBuilder.bind(fanoutQueue2).to(fanoutExchange);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
