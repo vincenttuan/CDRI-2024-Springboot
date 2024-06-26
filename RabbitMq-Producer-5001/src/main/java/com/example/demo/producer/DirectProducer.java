f5fb31039a1b820e292e54cdfacca193a2ad11d4
NTPU <NTPU@host.docker.internal>
2024-06-26 12:20:29 +0800

    java

---
+++ RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/DirectProducer.java
@@ -0,0 +1,15 @@
+package com.example.demo.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DirectProducer {
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	
+}
