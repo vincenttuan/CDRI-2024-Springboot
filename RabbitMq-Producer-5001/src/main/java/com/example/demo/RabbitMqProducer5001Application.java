b2ea4eb4904912f3cac2ee60240b8f7b5d3d41a1
NTPU <NTPU@host.docker.internal>
2024-06-26 11:26:46 +0800

    mq

---
+++ RabbitMq-Producer-5001/src/main/java/com/example/demo/RabbitMqProducer5001Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqProducer5001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqProducer5001Application.class, args);
+	}
+
+}
