e203915817f24f29e96a5fb15fdaef14839b8d77
NTPU <NTPU@host.docker.internal>
2024-06-26 12:30:59 +0800

    mq

--- RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/DirectProducer.java
+++ RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/DirectProducer.java
@@ -13,6 +13,8 @@ public class DirectProducer {
 	private RabbitTemplate rabbitTemplate;
 	
 	@GetMapping("/sendDirect")
+	// http://localhost:5001/sendDirect?routingKey=routingKey1
+	// http://localhost:5001/sendDirect?routingKey=routingKey2
 	public String sendDirect(@RequestParam String routingKey) {
 		String message = "Hello, Direct Exchange";
 		rabbitTemplate.convertAndSend("direct-exchange", routingKey, message);
