0b53dc4580ee4009006820acf60085c54b3fbeed
NTPU <NTPU@host.docker.internal>
2024-06-26 14:03:43 +0800

    mq

--- RabbitMq-Consumer-5002/src/main/java/com/example/demo/consumer/TopicComsumer.java
+++ RabbitMq-Consumer-5002/src/main/java/com/example/demo/consumer/TopicComsumer.java
@@ -1,5 +1,19 @@
 package com.example.demo.consumer;
 
-public class TopicComsumer {
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
 
+@Service
+public class TopicComsumer {
+	
+	@RabbitListener(queues = "topic-queue1")
+	public void receiveTopicQueue1(String message) {
+		System.out.println("Received from topic-queue1: " + message);
+	}
+	
+	@RabbitListener(queues = "topic-queue2")
+	public void receiveTopicQueue2(String message) {
+		System.out.println("Received from topic-queue2: " + message);
+	}
+	
 }
