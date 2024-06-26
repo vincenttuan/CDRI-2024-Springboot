9b5e385fd41f00fc89643503763c04cf5a02dd15
NTPU <NTPU@host.docker.internal>
2024-06-26 12:26:31 +0800

    mq

--- RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/DirectProducer.java
+++ RabbitMq-Producer-5001/src/main/java/com/example/demo/producer/DirectProducer.java
@@ -3,6 +3,7 @@ package com.example.demo.producer;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,13 @@ public class DirectProducer {
 	
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
+	
+	@GetMapping("/sendDirect")
+	public String sendDirect(@RequestParam String routingKey) {
+		String message = "Hello, Direct Exchange";
+		rabbitTemplate.convertAndSend("direct-exchange", routingKey, message);
+		return "Message [" + message + "] send to Direct Exchange with routing key: " + routingKey;
+	}
 	
 	
 }
